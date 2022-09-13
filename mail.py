78cd82e948c7a5a0c5bbb6f83932e9bb3c2307af
TelRich <okorogoodrich@gmail.com>
2022-09-14 00:20:11 +0100

    added funtion to mail.py script

--- mail.py
+++ mail.py
@@ -8,59 +8,67 @@ import pandas as pd
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 with open('token.json', 'rb') as token:
     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
 service = build('gmail', 'v1', credentials=creds)
 profile = service.users().getProfile(userId='me').execute()
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-with open('token.json', 'rb') as token:
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-
-service = build('gmail', 'v1', credentials=creds)
-
-filter = service.users().messages().list(userId = 'me', maxResults=10,
+def extract_transaction(maxResult=50, excel = False, csv=False):
+    # Filtering for transaction mails
+    filter = service.users().messages().list(userId = 'me', maxResults=maxResult,
                                          q = 'from:no_reply@accessbankplc.com \
                                              subject:AccessAlert Transaction Alert').execute()
 
-filter_id = filter['messages']
-id_lst = []
-for ids in filter_id:
-    id_lst.append(ids['id'])
+    # Extracting Ids from filtered mail
+    filter_id = filter['messages']
+    id_lst = []
+    for ids in filter_id:
+        id_lst.append(ids['id'])
     
-trans_lst = []
-for each_id in id_lst:
-    msgs = service.users().messages().get(userId = 'me', id = each_id).execute()
-    snippet = msgs['snippet']
-    main_body = msgs['payload']['body']['data']
-    main_body = main_body.replace('-', '+').replace('_', '/')
-    decode_msg = base64.b64decode(main_body)
-    soup = bs(decode_msg, 'lxml')
-    details = soup.find_all('tr')[7]
+    # Accessing trasaction summary
+    trans_lst = []
+    for each_id in id_lst:
+        msgs = service.users().messages().get(userId = 'me', id = each_id).execute()
+        snippet = msgs['snippet']
+        main_body = msgs['payload']['body']['data']
+        main_body = main_body.replace('-', '+').replace('_', '/')
+        decode_msg = base64.b64decode(main_body)
+        soup = bs(decode_msg, 'lxml')
+        details = soup.find_all('tr')[7]
+        
+        # Extracting required parameters from transaction summary
+        description = details.find_all('td')[6].text.replace('\r',' ').replace("\n"," ").strip()
+        reference_number = details.find_all('td')[8].text.replace('\r',' ').replace("\n"," ").strip()
+        trans_branch = details.find_all('td')[10].text.replace('\r',' ').replace("\n"," ").strip()
+        date = msgs['payload']['headers'][-1]['value']
+        amount = re.search('\d*\.+\d+', snippet).group()
+        acct_no = re.search('\d*\*+\d+', snippet).group()
+        
+        # Checking the type of transaction
+        if 'Credited' in snippet:
+            trans_type = 'Credited'
+        else:
+            trans_type = 'Debited'
+        
+        # Appending extracted parameters to a dictionary
+        trans_lst.append({
+            'amount': float(amount),
+            'a/c_number': acct_no,
+            'trans_type': trans_type,
+            'description': description,
+            'reference_number': reference_number,
+            'trans_branch': trans_branch,
+            'datetime': pd.to_datetime(date).tz_localize(None)
+        })
     
-    description = details.find_all('td')[6].text.replace('\r',' ').replace("\n"," ").strip()
-    reference_number = details.find_all('td')[8].text.replace('\r',' ').replace("\n"," ").strip()
-    trans_branch = details.find_all('td')[10].text.replace('\r',' ').replace("\n"," ").strip()
-    date = msgs['payload']['headers'][-1]['value']
-    amount = re.search('\d*\.+\d+', snippet).group()
-    acct_no = re.search('\d*\*+\d+', snippet).group()
+    # Assigning columns names and returning a dataframe of extracted parameters
+    cols_name = ['amount', 'a/c_number', 'trans_type', 'description', 'reference_number', 'trans_branch', 'datetime']
+    data = pd.DataFrame(trans_lst, columns=cols_name)
     
-    if 'Credited' in snippet:
-        trans_type = 'Credited'
-    else:
-        trans_type = 'Debited'
-    
-    
-    trans_lst.append({
-        'amount': amount,
-        'a/c_number': acct_no,
-        'trans_type': trans_type,
-        'description': description,
-        'reference_number': reference_number,
-        'trans_branch': trans_branch,
-        'datetime': date
-    })
-cols_name = ['amount', 'a/c_number', 'trans_type', 'description', 'reference_number', 'trans_branch', 'datetime']
-data = pd.DataFrame(trans_lst, columns=cols_name)
+    if excel:
+        data.to_excel("transaction.xlsx")
+    elif csv:
+        data.to_csv("transaction.csv", index=False)
+        
+    print(data)
 
-print(profile)
-print(data)
+if __name__ == '__main__':
+    extract_transaction(5)
